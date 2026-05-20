d7ed2e7cf91e537e395ff0b00cb29db0ceea53dd
joseluisdorado <joseluis.dorado@digipen.edu>
2026-05-20 12:37:08 +0200

    first commit

---
+++ src/Engine/Rasterizer/Rasterizer.h
@@ -0,0 +1,13 @@
+#ifndef CS200_RASTERIZER_H_
+#define CS200_RASTERIZER_H_
+
+// forward declaration of aevec2
+struct AEVec2;
+
+// Provided Framework
+#include "Color.h"			// Color
+#include "FrameBuffer.h"	// Frame buffer
+#include "Rounding.h"		// Rounding
+#include "DrawLine.h"		// Assignment 1 - Line Scan Conversion.
+
+#endif
---
+++ src/Levels/FreeDraw.cpp
@@ -0,0 +1,75 @@
+// ----------------------------------------------------------------------------
+// File: FreeDraw.cpp
+// Project: CS200 Assignment 1
+// Purpose: 
+// Author: Thomas Komair
+// Copyright DigiPen Institute of Technology, 2015. All rights reserved
+// ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+// INCLUDES
+#include <AEEngine.h>
+#include "..\Engine\Rasterizer\Rasterizer.h"
+#include "Common.h"
+
+namespace FreeDraw {
+	// ----------------------------------------------------------------------------
+	// GLOBALS AND DEFINITIONS
+	std::vector<std::pair<AEVec2, AEVec2>> gLineArray;
+	unsigned int	gCurrentLinePoint = 0;
+	// ----------------------------------------------------------------------------
+	// GAMESTATEFUNCTIONS
+	void Init()
+	{
+		gLineArray.clear();
+		gCurrentLinePoint = 0;
+	}
+	void Update()
+	{
+		KeyboardInput();
+		bool isGuiActive = ShowFrameworkMenu();
+
+		// current the mouse position to frame buffer space (origin at topleft)
+		AEVec2 mouseVP = AEGetMouseData().position + AEVec2((float)AEGetSysWindowWidth() / 2.0f, (float)AEGetSysWindowHeight() / 2.0f);
+
+		// clear all primitives
+		if (AEInputKeyTriggered('C'))
+		{
+			gLineArray.clear();
+			gCurrentLinePoint = 0;
+		}
+		if (!isGuiActive) {
+			// new line
+			if (AEInputMouseTriggered(AE_MOUSE_LEFT))
+			{
+				if (gCurrentLinePoint == 0 || AEInputKeyPressed(AE_KEY_CTRL)) {
+					gLineArray.push_back(std::pair<AEVec2, AEVec2>(mouseVP, mouseVP));
+					gCurrentLinePoint++;
+				}
+				else gCurrentLinePoint = 0;
+			}
+
+			// follow mouse
+			if (gCurrentLinePoint)
+			{
+				gLineArray.back().second = mouseVP;
+			}
+		}
+	}
+	void Render()
+	{
+		Rasterizer::FrameBuffer::Clear(Rasterizer::Color(1.0f, 1.0f, 1.0f, 1.0f));
+
+		// render all the lines
+		for (unsigned int i = 0; i < gLineArray.size(); ++i)
+		{
+			// get line endpoints
+			AEVec2& p0 = gLineArray[i].first;
+			AEVec2& p1 = gLineArray[i].second;
+
+			// Draw the line
+			Rasterizer::DrawLine(p0, p1, Rasterizer::Color());
+		}
+		Rasterizer::FrameBuffer::Present();
+	}
+}
